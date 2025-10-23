6bb63f8a51c700d8a32ed571f98ec221bda364c9
KITAE KIM <99127993+ki011127@users.noreply.github.com>
2025-10-23 19:48:19 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    unordered_map<string, int> m;
+    
+    for(int i = 0; i<clothes.size(); i++){
+        m[clothes[i][1]]++;
+    }
+    
+    for(auto &p : m){
+        answer*=(p.second+1);
+    }
+    
+    return answer-1;
+}
